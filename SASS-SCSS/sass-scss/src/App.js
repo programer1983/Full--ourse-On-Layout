e6934372a1a44e45743714c38e88ae9e84cb99e4
programer1983 <programer972@gmail.com>
2023-01-07 15:24:47 +0200

    SASS-SCSS: commit-4

--- SASS-SCSS/sass-scss/src/App.js
+++ SASS-SCSS/sass-scss/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <div className="block">
+        <p>Paragraph 1</p>
         <div className="block__text">
+          <p>Paragraph 2</p>
            Hello i am <span>Frilancer!</span>
         </div>
       </div>
