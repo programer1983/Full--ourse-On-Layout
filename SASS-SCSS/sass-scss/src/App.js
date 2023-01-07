a33dfb24f77574ca022a8a58722814bf7f1b589b
programer1983 <programer972@gmail.com>
2023-01-07 15:36:49 +0200

    SASS-SCSS: commit-7

--- SASS-SCSS/sass-scss/src/App.js
+++ SASS-SCSS/sass-scss/src/App.js
@@ -4,7 +4,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <div className="block">
+      <div className="block block-big">
         <a href="" className="link" >Google</a>
         <div className="block__text">
            Hello i am <span>Frilancer!</span>
