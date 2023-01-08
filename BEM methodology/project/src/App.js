c553ee37c25195af4aee0b256efc5f52c80e7761
programer1983 <programer972@gmail.com>
2023-01-08 09:25:36 +0200

    BEM methodology-6

--- BEM methodology/project/src/App.js
+++ BEM methodology/project/src/App.js
@@ -4,12 +4,10 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <nav className="menu">
-        <a href="com.ua" className="menu__link__item_active">HOME</a>
-        <a href="com.ua" className="menu__link">ABOUT US</a>
-        <a href="com.ua" className="menu__link">PORTFOLIO</a>
-        <a href="com.ua" className="menu__link">CONTACT</a>
-      </nav>
+      <div classNam="block">
+        <div classNam="about__title title"></div>
+        <div classNam="about__subtitle subtitle"> </div>
+      </div>
     </div>
   );
 }
