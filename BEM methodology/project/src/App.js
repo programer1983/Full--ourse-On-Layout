8d44a2ced7683f0332458575499dd669ad732fac
programer1983 <programer972@gmail.com>
2023-01-08 09:15:45 +0200

    BEM methodology-5

--- BEM methodology/project/src/App.js
+++ BEM methodology/project/src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <nav className="menu">
-        <a href="#" className="menu__link"></a>
-        <a href="#" className="menu__link"></a>
-        <a href="#" className="menu__link"></a>
-        <a href="#" className="menu__link"></a>
+        <a href="com.ua" className="menu__link__item_active">HOME</a>
+        <a href="com.ua" className="menu__link">ABOUT US</a>
+        <a href="com.ua" className="menu__link">PORTFOLIO</a>
+        <a href="com.ua" className="menu__link">CONTACT</a>
       </nav>
     </div>
   );
